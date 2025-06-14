9aa922917b25138baed2764db4f03b9327558eab
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-06-14 21:52:26 +0100

    chore(api): implement plate number scraping functionality with axios and cheerio

---
+++ api/features/lookup/scraper.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { ok, fail } from "tsfluent";
+
+export const scrapePlateNumber = async (plateNumber: string) => {
+  try {
+    const url = process.env.PLATENUMBER_VERIFICATION_URL as string;
+
+    const formData = new FormData();
+    formData.append("plateNumber", plateNumber);
+
+    const resposne = await axios.post(url, formData, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    const $ = cheerio.load(resposne.data);
+
+    const table = $("#print");
+
+    const makeText = table
+      .find("tr:contains('Vehicle Make') td:last-child span")
+      .text()
+      .trim();
+    const colorText = table
+      .find("tr:contains('Vehicle Color') td:last-child span")
+      .text()
+      .trim();
+
+    if (!makeText || !colorText) {
+      return fail("Vehicle details not found in response");
+    }
+
+    return ok({
+      make: makeText,
+      color: colorText,
+    });
+  } catch (error: unknown) {
+    return fail("Failed to retrieve plate number info");
+  }
+};
+
+scrapePlateNumber("FKJ254XA").then((result) => {
+  console.log(result);
+});
